043c84efa932a81ea455df296231e4ed0db2eeba
leon-good-life <leon.good.life@gmail.com>
2018-10-01 22:43:09 +0300

    Add initial cheatsheets

---
+++ react/metadata.js
@@ -0,0 +1,21 @@
+module.exports = {
+	title: 'React Cheatsheet',
+	file: 'react',
+	keywords: ['React Cheatsheet'],
+	color: '#61dafb',
+	sections: [
+		'create-react-app',
+		'stateless-component',
+		'class-component',
+		'properties-stateless',
+		'properties-class',
+		'children',
+		'architecture',
+		'state',
+		'react-router',
+		'react-redux-provider',
+		'react-redux-connect',
+		'ref',
+		'hoc'
+	]
+}
---
+++ redux/metadata.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'Redux Cheatsheet',
+  file: 'redux',
+  keywords: ['Redux Cheatsheet'],
+  color: '#764ABC',
+  sections: [
+    'action-types',
+    'action-creators',
+    'reducers',
+    'store',
+    'react-redux-provider',
+    'react-redux-connect',
+    'middleware'
+  ]
+}
