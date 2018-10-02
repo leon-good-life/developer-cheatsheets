a7a75400eb24cfd1872d9b9b964e327b565abf9b
leon-good-life <leon.good.life@gmail.com>
2018-10-02 22:11:30 +0300

    Update react metadata

--- react/metadata.js
+++ react/metadata.js
@@ -1,21 +1,22 @@
 module.exports = {
-	title: 'React Cheatsheet',
-	file: 'react',
-	keywords: ['React Cheatsheet'],
-	color: '#61dafb',
-	sections: [
-		'create-react-app',
-		'stateless-component',
-		'class-component',
-		'properties-stateless',
-		'properties-class',
-		'children',
-		'architecture',
-		'state',
-		'react-router',
-		'react-redux-provider',
-		'react-redux-connect',
-		'ref',
-		'hoc'
-	]
+  title: 'React Cheatsheet',
+  file: 'react',
+  keywords: ['React Cheatsheet'],
+  color: '#61dafb',
+  sections: [
+    'create-react-app',
+    'stateless-component',
+    'class-component',
+    'properties-stateless',
+    'properties-class',
+    'children',
+    'architecture',
+    'state',
+    'react-router',
+    'react-redux-provider',
+    'react-redux-connect',
+    'ref',
+    'hoc',
+    'render-props'
+  ]
 }
